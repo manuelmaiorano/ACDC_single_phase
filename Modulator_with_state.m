bc72468bc96744c355d3586ab06f0c783d0408ba
Manuel Maiorano <manuelmaiorano01@gmail.com>
2023-07-15 16:34:48 +0200

    add comments

--- Modulator_with_state.m
+++ Modulator_with_state.m
@@ -12,22 +12,23 @@ classdef Modulator_with_state < handle
         end
         
         function [qa, qb] = step(obj, u, t, h)
-            t_rel = t - (floor(t/h) * h);%istante di tempo relativo
-            u = bound(u, -1, 1);
+            t_rel = t - (floor(t/h) * h);%istante di tempo relativo all interno di h
+            u = bound(u, -1, 1);%limita la u tra -1 e 1
             
-            tau = abs(u) * h;
-            tau0 = h-tau;
-            if t_rel < tau0/2
-                obj.state = obj.state;
-                obj.state_first_tau0 = obj.state;
-            elseif t_rel > tau0/2 && t_rel < tau0/2+tau
+            tau = abs(u) * h;%percentuale  del tempo in cui qa qb = 1 0 o 0 1
+            tau0 = h-tau;%intervallo di tempo rimanenete qa qb 1 1 o 0 0
+            if t_rel < tau0/2%primo intervallo
+                obj.state = obj.state;%lasciamo invariati gli q
+                obj.state_first_tau0 = obj.state;%salviamo gli stati nel primo intervallo
+            elseif t_rel > tau0/2 && t_rel < tau0/2+tau%intervallo intermedio
                 
                 if u > 0
                     obj.state = [1, 0];
                 else
                     obj.state = [0, 1];
                 end
-            else
+            else%intervallo finale
+                %cummutiamo gli switch rispetto al primo intervallo
                 if isequal(obj.state_first_tau0, [0, 0])
                     obj.state = [1, 1];
                 else
